7db3b488dbc6b04c885b8b755a1f84c680552840
cadetsaad <cadetsaadsoomro@gmail.com>
2024-07-09 03:33:22 +0500

    firstcreated

---
+++ src/api/firstcollection/controllers/firstcollection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * firstcollection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::firstcollection.firstcollection');
---
+++ src/api/firstcollection/routes/firstcollection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * firstcollection router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::firstcollection.firstcollection');
